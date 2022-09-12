bba03830ddbe97e9e78d62cbe2ce138fb1b92c5d
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-09-12 10:46:28 -0700

    awdawdsd

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
 
 class Luauth {
-
+    
 }
 
 Luauth.login = (API) => new Luauth(API);
-module.exports = Luauth
+module.exports = Luauth;
+exports = module.exports;
