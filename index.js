c87d85b70ee90ba965b889d4f9bbb543162a5467
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-09-16 15:58:39 -0700

    worked on it a tiny tiny bit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const axios = require('axios');
 
 class Luauth {
-    
+    constructor(KEY) {
+        this.#API_KEY = KEY;
+
+
+    }
+    #BaseURL = "https://api.luauth.xyz/";
+    request = (method = "GET", endpoint, data) => {
+
+    }
 }
 
-Luauth.login = (API) => new Luauth(API);
+Luauth.login = (KEY) => new Luauth(KEY);
 module.exports = Luauth;
 
 exports = module.exports;
