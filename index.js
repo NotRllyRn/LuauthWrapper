be3117934a29b526a4d388be460cc76720d670c1
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-09-23 12:40:27 -0700

    slowlyyyy working on it

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-const axios = require('axios');
+const axios = (...args) => import('axios').then(({default: axios}) => axios(...args));
 
 class Luauth {
     constructor(KEY) {
-        this.#API_KEY = KEY;
+        this.#Key = KEY;
 
+        let data = this.#baseRequest('get', 'status');
+        if (!data.active) throw new Error(data.message);
 
+        
+
+        this.#Version = data.gateway_version;
+        return this
     }
     #BaseURL = "https://api.luauth.xyz/";
-    request = (method = "GET", endpoint, data) => {
-
+    #Key;
+    #Version;
+    #baseRequest = async (method = "get", endpoint, data) => {
+        return new Promise((resolve, reject) => {
+            axios({
+                method: method,
+                url: this.#BaseURL + endpoint,
+                data: (method == "post" ? data && JSON.stringify(data) : null),
+                headers: {
+                    "Authorization": this.#Key,
+                    "Content-Type": "application/json"
+                },
+            }).then(res => {
+                resolve(res.data);
+            }).catch(err => {
+                reject(err);
+            });
+        });
     }
+    #request = (method, endpoint, data) => this.#baseRequest(method, `${this.#Version}/${endpoint}`, data);
 }
 
 Luauth.login = (KEY) => new Luauth(KEY);
-module.exports = Luauth;
+export default Luauth;
 
 exports = module.exports;
